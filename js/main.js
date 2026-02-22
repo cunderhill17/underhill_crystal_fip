b05e3414fcf16c2edce0c307e803367270eefa9b
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-02-22 17:42:06 -0500

    Update Navigation Functionality
    This commit updated the functionality for the main nav so it was only being called on customer pages as well as added the functionality for the vertical nav on admin pages

--- js/main.js
+++ js/main.js
@@ -166,6 +166,14 @@ const productData = {
   }
 };
 
+// Admin Vertical Nav variables
+const openVerticalNav = document.querySelector('#openVerticalNav');
+const closeVerticalNav = document.querySelector('#closeVerticalNav');
+const verticalNav = document.querySelector('#vertical-nav');
+
+// Variable for dropdowns on Vertical Nav menu Admin pages
+const toggleBtns = document.querySelectorAll('.dropdown-toggle'); 
+
 //Contact Map Variables 
 let map;
 const searchBtn = document.querySelector('#search-btn');
@@ -373,13 +381,31 @@ function changePreview(e) {
   lgImage.innerHTML = `<img src="${imgPath}">`;
 }
 
+//Functions to open and close the vertical nav bar on admin pages
+function openVerticalNavMenu() {
+    verticalNav.classList.add('open');
+}
+
+function closeVerticalNavMenu() {
+    verticalNav.classList.remove('open');
+}
+
+//Function to toggle the submenu open and close in the vertical nav menu on admin pages
+function toggleSubMenu(e) {
+    const button = e.currentTarget;
+    const menu = button.nextElementSibling;
+
+    menu.classList.toggle('open');
+}
 
 
 
 /* ---Event Handlers--- */
 
 //Hamburger Menu Event Handler
-menuBtn.addEventListener('click', toggleNav);
+if (menuBtn) {
+  menuBtn.addEventListener('click', toggleNav);
+}
 
 //Product Card Event Handler
 
@@ -392,4 +418,17 @@ if (flavors) {
 if (searchBtn) {
    searchBtn.addEventListener('click', handleAddressSearch); 
 }
+
+//Admin Vertical Nav Bar Event handler
+if (openVerticalNav) {
+  openVerticalNav.addEventListener('click', openVerticalNavMenu);
+  closeVerticalNav.addEventListener('click', closeVerticalNavMenu);
+}
+
+// Admin event handler to add a click event to the button to toggle the sub menu
+if (toggleBtns) {
+  toggleBtns.forEach(toggleBtn => toggleBtn.addEventListener('click', toggleSubMenu));
+}
+
+
 
