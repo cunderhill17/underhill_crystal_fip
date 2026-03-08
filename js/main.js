cfeb1b84702f81cfcf5508314a0770013063169d
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-03-08 17:10:13 -0400

    update main.js
    This commit updates the main.js file so that the productInfo object is now dynamically populated using data from the database

--- js/main.js
+++ js/main.js
@@ -161,6 +161,9 @@ const productData = {
   }
 };
 
+//Object that will hold data gathered from the database
+let productInfo = {};
+
 // Admin Vertical Nav variables
 const openVerticalNav = document.querySelector('#openVerticalNav');
 const closeVerticalNav = document.querySelector('#closeVerticalNav');
@@ -457,6 +460,74 @@ function displayViewSingleProduct() {
       individualProduct.classList.remove('hidden');
     }
 }
+
+//Function to get data returned from php script -- PRODUCTS
+async function getProducts() {
+    const response = await fetch('php/actions/viewProducts.php');
+    const products = await response.json();
+
+    products.forEach(product => {
+        const productKey = product.id;
+
+        productInfo[productKey] = {
+            flavor: product.product_name,
+            description: product.product_description,
+            ingredients: product.product_ingredients,
+            price: product.product_price,
+            salePrice: product.product_sale_price,
+            stock: product.product_stock,
+            images: [
+                { 
+                  alt: product.product_image_alt,
+                  image: `images/${product.product_image}`,
+                },
+                {
+                  alt: product.product_pre_1_alt,
+                  image:`images/${product.product_pre_1}`,
+                },
+                {
+                  alt: product.product_pre_2_alt,
+                  image:`images/${product.product_pre_2}`,
+                },
+                {
+                  alt: product.product_pre_3_alt,
+                  image:`images/${product.product_pre_3}`,
+                },
+                {
+                  alt: product.product_pre_4_alt,
+                  image:`images/${product.product_pre_4}`,
+                },
+              ],
+            otherFlavors: [
+              { 
+                id: 'fruit-redCurrent',
+                image:'images/fruit-red-currant.svg',
+              },
+              {
+                id: 'fruit-darkCherry',
+                image:'images/fruit-cherries.svg',
+              },
+              {
+                id: 'fruit-bloodOrange',
+                image:'images/fruit-orange-slice.svg',
+              },
+              {
+                id: 'fruit-pomegranate',
+                image:'images/fruit-pomegranate.svg',
+              },
+              {
+                id: 'fruit-bergamot',
+                image:'images/fruit-bergamot.svg',
+              },
+            ]
+        };
+    });
+
+}
+
+getProducts();
+
+
 
 
 
